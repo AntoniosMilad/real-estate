36d14da306eee10b998b46e38ffefa629dfc59ae
ahmed osama <ahmedosama@192.168.1.8>
2024-09-29 22:18:47 +0300

    finish single property desktop

---
+++ src/Components/Property/PlansDetails.tsx
@@ -0,0 +1,107 @@
+import { Property } from "@/data/properties";
+import React from "react";
+
+export default function PlansDetails(props: {
+  singleProperty: Property | null;
+}) {
+  return (
+    <section className="flex flex-col gap-6 w-[80%]">
+      <article className="flex flex-row gap-6 ">
+        <div className="border-[1px] border-gray-200 rounded-md w-[15%] h-[255px] flex flex-col items-center py-5">
+          <p className="text-[#002E5D] text-[18px] font-bold">Payment Plans</p>
+          <div className="flex flex-col gap-3 mt-3">
+            <div className="relative">
+              <span className="text-black text-xl font-bold">
+                {props.singleProperty?.Price.monthlyInstallment} EGP
+              </span>
+              <p className="text-gray-400 text-[12px]">Max Price :</p>
+            </div>
+            <div>
+              <p className="text-black text-xl font-bold flex items-center gap-1">
+                {props.singleProperty?.Price.TotalPrice} EGP
+              </p>
+              <p className="text-gray-400 text-[12px]">Max Price :</p>
+            </div>
+          </div>
+        </div>
+        <div className="border-[1px] border-gray-200 rounded-md w-[30%] h-[255px] flex flex-col p-5">
+          <p className="text-[#002E5D] text-[18px] font-bold">Details</p>
+          <div className="flex gap-9 mt-5">
+            <ol className="text-black list-disc list-inside space-y-3">
+              <li>Home</li>
+              <li>About us</li>
+              <li>Properties</li>
+            </ol>
+            <ol className="text-black list-disc list-inside space-y-3">
+              <li>Investment</li>
+              <li>Expert Advice</li>
+              <li>Contact us</li>
+            </ol>
+          </div>
+        </div>
+        <div className="border-[1px] border-gray-200 rounded-md w-[50%] h-[255px] flex flex-col p-5">
+          <p className="text-[#002E5D] text-[18px] font-bold">Amenities</p>
+          <div className="flex gap-9 mt-5">
+            <ol className="text-black list-disc list-inside space-y-3">
+              <li>Home</li>
+              <li>About us</li>
+              <li>Properties</li>
+            </ol>
+            <ol className="text-black list-disc list-inside space-y-3">
+              <li>Investment</li>
+              <li>Expert Advice</li>
+              <li>Contact us</li>
+            </ol>
+            <ol className="text-black list-disc list-inside space-y-3">
+              <li>Investment</li>
+              <li>Expert Advice</li>
+              <li>Contact us</li>
+            </ol>
+            <ol className="text-black list-disc list-inside space-y-3">
+              <li>Investment</li>
+              <li>Expert Advice</li>
+              <li>Contact us</li>
+            </ol>
+          </div>
+        </div>
+      </article>
+      <article>
+        <p className="text-[#002E5D] text-[18px] font-bold">About Apartment</p>
+        <p className="my-3 w-[60%] text-black text-[15px] text-justify leading-[27px]">
+          A 2 bedroom Apartment in ZED by Ora Developers. The Apartment size is
+          127 m2 with 3 bathrooms and a terrace of 38 m2 . This property will be
+          ready for delivery finished by 2028-01-01. Unique Selling Points of
+          Zed West Y Towers Mall Y Tower Mall is placed at the crown of Zed Park
+          El-Sheikh Zayed. It grants the guests access to boundless shopping
+          experiences, as well as food and beverage venues. It consists of two
+          floors dedicated to dazzling commercial journeys. It is also a
+          hospitality gateway and a fine dining experience.  Zed Park The
+          dynamic soul of ZED Park has the power to ignite energy and happiness
+          for everyone. It is stretched over 65 acres of breathtaking greenery.
+          ZED Park offers you a wide range of restaurants, cafes, diverse
+          entertainment venues, workspaces, and yoga spaces. Zed Sports Complex
+          For all sports enthusiasts, ZED El Sheikh Zayed features a sports
+          complex with a variety of physical activities. It also includes court
+          rentals, academies, and many other services.
+        </p>
+        <p className="my-3 w-[60%] text-black text-[15px] text-justify leading-[27px]">
+          A 2 bedroom Apartment in ZED by Ora Developers. The Apartment size is
+          127 m2 with 3 bathrooms and a terrace of 38 m2 . This property will be
+          ready for delivery finished by 2028-01-01. Unique Selling Points of
+          Zed West Y Towers Mall Y Tower Mall is placed at the crown of Zed Park
+          El-Sheikh Zayed. It grants the guests access to boundless shopping
+          experiences, as well as food and beverage venues. It consists of two
+          floors dedicated to dazzling commercial journeys. It is also a
+          hospitality gateway and a fine dining experience.  Zed Park The
+          dynamic soul of ZED Park has the power to ignite energy and happiness
+          for everyone. It is stretched over 65 acres of breathtaking greenery.
+          ZED Park offers you a wide range of restaurants, cafes, diverse
+          entertainment venues, workspaces, and yoga spaces. Zed Sports Complex
+          For all sports enthusiasts, ZED El Sheikh Zayed features a sports
+          complex with a variety of physical activities. It also includes court
+          rentals, academies, and many other services.
+        </p>
+      </article>
+    </section>
+  );
+}
--- src/app/[property]/page.tsx
+++ src/app/[property]/page.tsx
@@ -1,10 +1,15 @@
 "use client";
-import PropertySlider from "@/Components/Property/PropertySlider";
+import PropertySliderTwo from "@/Components/Property/PropertySliderTwo";
 import SuggestedProperties from "@/Components/Property/SuggestedProperties";
 import { properties, Property } from "@/data/properties";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import location from "@/assets/SVG/location.svg";
+import PlansDetails from "@/Components/Property/PlansDetails";
+import status from "@/assets/SVG/Pricelevel.svg";
+import whats from "@/assets/SVG/whatsBlue.svg";
+import phone from "@/assets/SVG/Phone.svg";
 
 export default function PropertyDetails({
   params,
@@ -18,10 +23,9 @@ export default function PropertyDetails({
     );
     if (findedProperty) setSingleProperty(findedProperty);
   }, [params.property]);
-  console.log(singleProperty);
 
   return (
-    <section className="pt-[80px] h-[2000px]">
+    <section className="pt-[80px] min-h-[2000px] h-auto">
       <div className="mt-8">
         <p className="text-black flex gap-2">
           <Link href="/" className="font-bold">
@@ -29,8 +33,7 @@ export default function PropertyDetails({
           </Link>{" "}
           {decodeURIComponent(params.property)}
         </p>
-        <PropertySlider />
-        <div className="w-full h-[300px]">
+        <div className="w-full my-6">
           <div className="flex flex-row items-center gap-6">
             <div className="flex justify-center items-center shadow-gray-300/30 shadow-lg rounded-full  w-[100px] h-[100px]">
               <div className="relative w-[100%] h-[30%]">
@@ -41,31 +44,58 @@ export default function PropertyDetails({
                 />
               </div>
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col gap-1">
               <p className="text-[#002E5D] text-2xl font-bold">
                 Apartment for sale in {decodeURIComponent(params.property)} with
                 2 Bedrooms
               </p>
-              <p className="text-gray-400 font-semibold text-[14px]">
-                {singleProperty?.Location}
-              </p>
-              <div className="flex items-end gap-6 mt-1">
-                <div className="relative">
-                  <p className="text-gray-400 text-[10px] before:content-['Prices_start_from:_'] before:block before:text-gray-400 before:text-[10px]">
-                    <span className="text-black text-xl font-bold">
-                      {singleProperty?.Price.monthlyInstallment} EGP
-                    </span>
-                  </p>
-                </div>
-
-                <p className="text-black text-xl font-bold flex items-center gap-1">
-                  <span className="text-gray-400 text-[12px]">Max Price :</span>{" "}
-                  {singleProperty?.Price.TotalPrice} EGP
+              <div className="flex items-center gap-1">
+                <Image src={location} alt="location" width={16} height={16} />
+                <p className="text-gray-400 font-semibold text-[14px]">
+                  {singleProperty?.Location}
                 </p>
               </div>
             </div>
           </div>
         </div>
+        {/* <PropertySlider /> */}
+        <PropertySliderTwo />
+        <div className="flex my-10 w-full">
+          <PlansDetails singleProperty={singleProperty} />
+          <div className="border-[1px] border-gray-200 rounded-md flex-grow h-[400px] flex flex-col p-5 shadow-md shadow-gray-300/30">
+            <div className="py-1  rounded-[20px] bg-gray-200 mb-4 w-[100px] flex flex-row items-center gap-2">
+              <div className=" w-5 h-5 rounded-full flex justify-center items-center">
+                <Image src={status} alt="Bedrooms" width={10} height={10} />
+              </div>
+              <p className="text-[12px] text-white font-bold">Good Price</p>
+            </div>
+            <p className="text-gray-400 text-base">Total</p>
+            <p className="text-black text-xl font-bold flex items-center gap-1">
+              {singleProperty?.Price.TotalPrice} EGP
+            </p>
+            <div className="flex items-center gap-5 my-3">
+              <div className="bg-gray-200 w-10 h-10 flex justify-center items-center rounded-full">
+                <Image src={phone} alt="Bedrooms" width={25} height={15} />
+              </div>
+              <div className="bg-green-400 w-10 h-10 flex justify-center items-center rounded-full">
+                <Image src={whats} alt="Bedrooms" width={25} height={15} />
+              </div>
+            </div>
+            <hr className="my-8" />
+            <div>
+              <p className="text-[18px] text-black mb-2">
+                Ready to Invest Smartly?
+              </p>
+              <p className="text-[#818181]">
+                Get expert insights tailored to your needs.
+              </p>
+              <button className="w-[200px] text-center mx-auto font-semibold h-[48px] border-[1px] border-[#002E5D] text-[#002E5D] flex justify-center items-center bg-[#FFFFFF] rounded-md mt-[20px]">
+                Book Free Consultation
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr className="my-10" />
         <SuggestedProperties />
       </div>
     </section>
